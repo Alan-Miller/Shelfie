690eca84a8ed06122457c96bdf21b4733ee4501e
Alan-Miller <alan.ashman@gmail.com>
2018-01-29 12:33:08 -0700

    working header with conditional rendering

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
+
 function Header(props) {
-  const { path } = props.match;
+  // <h4>{pathname}</h4>
+  const { pathname } = props.location;
+  const pathArray = pathname.split('/').filter(c => c !== '');
+  const [unused1, shelf, unused2, bin] = pathArray;
+
   return (
     <div className="header">
       <Link to="/">
-        <div className="logo_container">
-          <div className="header_logo"></div>
-          <h1>SHELFIE</h1>
-          <h4>{props.location.pathname}</h4>
-        </div>
+        {
+          bin ?
+          <div>
+            <div className="logo_container bin-container">
+              <div className="shelfie_logo"></div>
+            </div>
+            <div className="bin_text">Bin {bin}</div>
+            <div className="shelf_text">Shelf {shelf}</div>
+          </div>
+          : 
+          shelf ?
+          <div>
+            <div className="logo_container shelf-container">
+              <div className="shelfie_logo"></div>
+            </div>
+            <div className="shelf_text">Shelf {shelf}</div>
+          </div>
+          : 
+          <div className="logo_container shelfie-container">
+            <div className="shelfie_logo"></div>
+            <h1 className="shelfie_text">SHELFIE</h1>
+          </div>
+        }
       </Link>
     </div>
   )
